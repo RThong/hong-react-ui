d7524e104d82774e9fcdf638a5eabcea800711e7
brant <403216075@qq.com>
2021-02-23 16:24:59 +0800

    [chore]添加lint相关内容

---
+++ .eslintrc.js
@@ -0,0 +1,27 @@
+module.exports = {
+  settings: {},
+  extends: [
+    'alloy',
+    'alloy/react',
+    'alloy/typescript',
+    'plugin:react-hooks/recommended',
+  ],
+  // extends: 'airbnb-base',
+  // plugins: ['promise'],
+  env: {
+    // 你的环境变量（包含多个预定义的全局变量）
+    //
+    // browser: true,
+    // node: true,
+    // mocha: true,
+    // jest: true,
+    // jquery: true
+  },
+  globals: {
+    // 你的全局变量（设置为 false 表示它不允许被重新赋值）
+  },
+  rules: {
+    // 自定义你的规则
+    'no-param-reassign': 0,
+  },
+};
