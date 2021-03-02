36d3b83b9b2f72e39a53fc7a01fd5f276abdbffc
RThong <403216075@qq.com>
2021-03-02 21:59:22 +0800

    [chore]调整export

--- src/input/index.tsx
+++ src/input/index.tsx
@@ -1,3 +1,4 @@
+import Input from './Input';
 import Search from './Search';
 import TextArea from './TextArea';
 
@@ -5,6 +6,4 @@ export { InputProps, InputRef } from './Input';
 export { SearchProps, SearchRef } from './Search';
 export { TextAreaProps, TextAreaRef } from './TextArea';
 
-const Input = Object.assign(import('./Input'), { Search, TextArea });
-
-export default Input;
+export default Object.assign(Input, { Search, TextArea });
