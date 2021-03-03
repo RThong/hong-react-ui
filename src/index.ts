f2fd9b93887ceb006b32f54921a3c2409eb9d0e1
brant <403216075@qq.com>
2021-03-03 17:22:51 +0800

    [fix]修复大小写

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,3 @@
 export { default as Button } from './button';
 export { default as Input } from './input';
-export { default as Checkbox } from './Checkbox';
+export { default as Checkbox } from './checkbox';
