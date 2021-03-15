2818d1649627a1e6173147ca0942bb6a38c9dc84
RThong <403216075@qq.com>
2021-03-16 00:04:59 +0800

    [feat]初步实现Transition需求

--- src/transition/demo/base.tsx
+++ src/transition/demo/base.tsx
@@ -2,15 +2,34 @@
  * title: 基本用法
  * desc: 最基本的用法。
  */
-import React from 'react';
+import React, { useState } from 'react';
 import { Button, Transition } from 'hong-react-ui';
 import 'hong-react-ui/dist/index.css';
 
 const Demo = () => {
+  const [visible, setVisible] = useState(false);
   return (
-    <Transition>
-      <div>transition</div>
-    </Transition>
+    <div>
+      <Button onClick={() => setVisible((v) => !v)}>click</Button>
+      <Transition
+        visible={visible}
+        enter={{ width: 200 }}
+        leave={{ width: 20 }}
+      >
+        {({ className: motionClassName, style: motionStyle }, motionRef) => (
+          <div
+            className={motionClassName}
+            style={{
+              background: 'green',
+              height: 30,
+              width: 20,
+              ...motionStyle,
+            }}
+            ref={motionRef}
+          />
+        )}
+      </Transition>
+    </div>
   );
 };
 
--- src/transition/index.tsx
+++ src/transition/index.tsx
@@ -1,17 +1,69 @@
-import React, { CSSProperties } from 'react';
+import React, { useEffect, useRef, useState } from 'react';
 import classnames from 'classnames';
 import { createScopedClasses } from '@/utils';
 
 interface TransitionProps {
-  aaa?: string;
+  enter: React.CSSProperties;
+  leave: React.CSSProperties;
+  visible: boolean;
+  children?: (
+    props: {
+      visible?: boolean;
+      className?: string;
+      style?: React.CSSProperties;
+      [key: string]: any;
+    },
+    ref: (node: any) => void,
+  ) => React.ReactElement;
 }
 
 const sc = createScopedClasses('transition');
 
-const Transition: React.FC<TransitionProps> = (props) => {
-  const { children } = props;
+const Transition = (props: TransitionProps) => {
+  const { children, enter, leave, visible } = props;
 
-  return <div className={classnames(sc())}>{children}</div>;
+  const [animationVisible, setAnimationVisible] = useState(visible);
+
+  const [contentStyle, setContentStyle] = useState<React.CSSProperties>({});
+
+  useEffect(() => {
+    visible && setAnimationVisible(visible);
+  }, [visible]);
+
+  useEffect(() => {
+    setTimeout(() => {
+      setContentStyle(visible ? enter : leave);
+    }, 16);
+  }, [visible, enter, leave]);
+
+  const ref = useRef<HTMLDivElement>(null);
+
+  useEffect(() => {
+    const fn = () => {
+      // setAnimationVisible(false);
+      console.log('【visible】', visible);
+      if (!visible) {
+        setAnimationVisible(false);
+      }
+    };
+    const aaa = ref.current;
+
+    aaa?.addEventListener('transitionend', fn);
+    return () => {
+      aaa?.removeEventListener('transitionend', fn);
+    };
+  }, [visible]);
+
+  return children(
+    {
+      className: classnames(sc()),
+      style: {
+        ...contentStyle,
+        display: animationVisible ? undefined : 'none',
+      },
+    },
+    ref,
+  );
 };
 
 export default Transition;
